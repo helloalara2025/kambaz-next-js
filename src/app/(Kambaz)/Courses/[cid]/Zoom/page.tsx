d2cf85a3e635d3abd5c88f08007b1215d3088d17
alara <alarahkk@gmail.com>
2025-12-05 22:35:52 -0500

    a43

--- src/app/(Kambaz)/Courses/[cid]/Zoom/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Zoom() {
-  return (
-    <div id="wd-zoom">
-      <h1>Zoom</h1>
-      <p>Zoom will be integrated later... placeholder page</p>
-    </div>
-  );
-}
--- src/app/Labs/Lab2/page 2.tsx
+++
@@ -1,6 +0,0 @@
-// export default function Lab2() {
-//   return (
-//     <div id="wd-lab3">
-//       <h2>Lab 2</h2>
-//     </div>
-// );}
