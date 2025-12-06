7a3a57b99bed71a01ced5fe153f330f507b633b4
alara <alarahkk@gmail.com>
2025-12-05 23:15:38 -0500

    Fix dashboard course imports

--- app/(Kambaz)/Dashboard/Courses/[cid]/Home/page.tsx
+++ app/(Kambaz)/Dashboard/Courses/[cid]/Home/page.tsx
@@ -1,9 +1,7 @@
-// src/app/(Kambaz)/Courses/[cid]/Home/page.tsx ---
-/* This file */
 // This creates the home screen combining the course status and modules together.
 // Combines the Course Status with the Modules to create the Home screen for the course screen.
-import Modules from "../Modules/page";
-import CourseStatus from "./coursehomepagestatus";
+import Modules from "@/app/(Kambaz)/Courses/[cid]/Modules/page";
+import CourseStatus from "@/app/(Kambaz)/Courses/[cid]/Home/coursehomepagestatus";
 import React from 'react';
 
 export default function Home() {
