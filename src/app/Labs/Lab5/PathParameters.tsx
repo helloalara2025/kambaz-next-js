a1c463db7e9ce257d560d0a556de453090126b37
alara <alarahkk@gmail.com>
2025-12-14 02:00:26 +0000

    Add multiply and divide buttons to Lab5 PathParameters

--- src/app/Labs/Lab5/PathParameters.tsx
+++ src/app/Labs/Lab5/PathParameters.tsx
@@ -12,22 +12,22 @@ export default function PathParameters() {
   return (
     <div>
       <h4>Path Parameters</h4>
-      <FormControl 
-        className="mb-2" 
-        id="wd-path-parameter-a" 
-        type="number" 
+      <FormControl
+        className="mb-2"
+        id="wd-path-parameter-a"
+        type="number"
         defaultValue={a}
         onChange={(e) => setA(e.target.value)}
       />
-      <FormControl 
-        className="mb-2" 
-        id="wd-path-parameter-b" 
-        type="number" 
+      <FormControl
+        className="mb-2"
+        id="wd-path-parameter-b"
+        type="number"
         defaultValue={b}
         onChange={(e) => setB(e.target.value)}
       />
 
-      <a 
+      <a
         className="btn btn-primary me-2"
         id="wd-path-parameter-add"
         href={`${HTTP_SERVER}/lab5/add/${a}/${b}`}
@@ -35,12 +35,28 @@ export default function PathParameters() {
         Add {a} + {b}
       </a>
 
-      <a 
+      <a
         className="btn btn-danger me-2"
-        id="wd-path-parameter-subtract" 
+        id="wd-path-parameter-subtract"
         href={`${HTTP_SERVER}/lab5/subtract/${a}/${b}`}
       >
         Substract {a} - {b}
+      </a>
+
+      <a
+        className="btn btn-warning me-2"
+        id="wd-path-parameter-multiply"
+        href={`${HTTP_SERVER}/lab5/multiply/${a}/${b}`}
+      >
+        Multiply {a} * {b}
+      </a>
+
+      <a
+        className="btn btn-success"
+        id="wd-path-parameter-divide"
+        href={`${HTTP_SERVER}/lab5/divide/${a}/${b}`}
+      >
+        Divide {a} / {b}
       </a>
 
       <hr />
